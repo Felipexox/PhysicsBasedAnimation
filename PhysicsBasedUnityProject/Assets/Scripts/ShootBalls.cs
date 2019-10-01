9a39974cdb944bc0e0748bea8d05acd23481dfda
Felipexox <felipe.arruda.oktagon@gmail.com>
2019-10-01 13:14:49 -0300

    [Commit]   - Simple sample skeleton

---
+++ PhysicsBasedUnityProject/Assets/Scripts/ShootBalls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ShootBalls : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Prefab;
+
+    [SerializeField] private float m_Force;
+
+    [SerializeField] private KeyCode m_KeyToCreate;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(m_KeyToCreate))
+        {
+            Ray tRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            GameObject tObjectInstantiated = Instantiate(m_Prefab, tRay.origin, m_Prefab.transform.rotation);
+            
+            tObjectInstantiated.GetComponent<Rigidbody>().AddForce(tRay.direction * m_Force);
+        }
+    }
+}
---
+++ PhysicsBasedUnityProject/Assets/Scripts/SkeletonCore/TorqueJoint.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorqueJoint : MonoBehaviour
+{
+    [SerializeField] private float m_TorqueJoint;
+
+    [SerializeField] private float m_DownForce;
+
+    [SerializeField] private Rigidbody m_RigidBody;
+
+    [SerializeField] private float m_DeltaTime;
+
+    [SerializeField] private bool m_LocalAngle;
+
+    [SerializeField] private Transform m_Joint;
+
+    [SerializeField] private bool m_Debug;
+
+    [SerializeField] private float m_Distance;
+
+    [SerializeField] private bool m_StayHigh;
+
+    [SerializeField] private bool m_UseAddTorque;
+
+    [SerializeField] private bool m_Normal;
+    
+    private Vector3 m_TorqueRotation;
+    
+    private void Update()
+    {
+        float tHorizontal = Input.GetAxis("Horizontal");
+        float tVertical = Input.GetAxis("Vertical");
+
+        Vector3 m_NewTorque = Vector3.Lerp(m_TorqueRotation, GetRotationInverse(), Time.deltaTime * m_DeltaTime);
+
+        if (m_Debug)
+        {
+            Debug.Log(m_TorqueRotation);
+            Debug.Log(Vector3.Distance(m_NewTorque, m_TorqueRotation));
+        }
+
+        if (Vector3.Distance(m_NewTorque, m_TorqueRotation) >= m_Distance)
+        {
+            m_TorqueRotation = m_NewTorque;
+            if(m_UseAddTorque)
+                m_RigidBody.AddTorque( m_StayHigh?GetRotationInverse() * 1000 : m_TorqueRotation * m_TorqueJoint);
+            else
+                m_RigidBody.angularVelocity = ( m_StayHigh?GetRotationInverse() * 20 : m_TorqueRotation * m_TorqueJoint);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))    
+            m_RigidBody.AddForce(new Vector3(tHorizontal, 1, tVertical) * Mathf.Abs(m_DownForce));
+        if(m_Normal)
+            m_RigidBody.AddForce(Vector3.down * 9.81f);
+
+    }
+
+    Vector3 GetRotationInverse()
+    {
+        if(!m_LocalAngle)
+            return new Vector3(-GetAngleBetween(m_RigidBody.transform.up, Vector3.forward),0,GetAngleBetween(m_RigidBody.transform.up, Vector3.right));
+        else
+            return new Vector3(-GetAngleBetween(m_RigidBody.transform.up, m_Joint.forward),0,GetAngleBetween(m_RigidBody.transform.up, m_Joint.right));
+    }
+
+    float GetAngleBetween(Vector3 a, Vector3 b)
+    {
+        Vector3 multiple = Vector3.Scale(a, b);
+        float scalar = multiple.x + multiple.y + multiple.z;
+        float module = Vector3.Distance(a, Vector3.zero) * Vector3.Distance(b, Vector3.zero);
+
+        return scalar / module;
+
+    }
+}
